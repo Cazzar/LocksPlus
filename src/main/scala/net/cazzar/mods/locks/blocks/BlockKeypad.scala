9ce235896e17cc4cd5b61b264e4129ec3fc20534
Cayde Dixon <cayde.dixon@gmail.com>
2014-03-01 22:59:59 +1100

    Hitboxes  proper TE syncing,  redstone activation. TODO: Textures

--- src/main/scala/net/cazzar/mods/locks/blocks/BlockKeypad.scala
+++ src/main/scala/net/cazzar/mods/locks/blocks/BlockKeypad.scala
@@ -1,19 +1,29 @@
 package net.cazzar.mods.locks.blocks
 
-import net.minecraft.block.{BlockFurnace, Block}
+import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.world.{IBlockAccess, World}
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.init.Blocks
 import net.minecraft.tileentity.TileEntity
 import net.cazzar.mods.locks.blocks.tile.TileKeypad
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.ItemStack
-import net.minecraft.util.MathHelper
+import net.minecraft.util.{AxisAlignedBB, MathHelper}
 import net.minecraftforge.common.util.ForgeDirection
+import net.minecraft.server.MinecraftServer
 
 class BlockKeypad extends Block(Material.rock) {
     setBlockName("lock_keypad")
+    setTickRandomly(true)
+
+    override def breakBlock(world: World, x: Int, y: Int, z: Int, block: Block, meta: Int): Unit = {
+        val keypad: TileKeypad = world.getTileEntity(x, y, z).asInstanceOf[TileKeypad]
+
+        world.notifyBlocksOfNeighborChange(x, y, z, this)
+        world.notifyBlocksOfNeighborChange(x + keypad.facing.offsetX, y + keypad.facing.offsetY, z + keypad.facing.offsetZ, this)
+
+        super.breakBlock(world, x, y, z, block, meta)
+    }
 
     override def hasTileEntity(metadata: Int): Boolean = true
 
@@ -24,14 +34,24 @@ class BlockKeypad extends Block(Material.rock) {
     override def isOpaqueCube: Boolean = false
 
     override def onBlockActivated(world: World, blockX: Int, blockY: Int, blockZ: Int, player: EntityPlayer, p_149727_6_ : Int, playerX: Float, playerY: Float, playerZ: Float): Boolean = {
-        false
+        if (!world.isRemote) {
+            world.getTileEntity(blockX, blockY, blockZ).asInstanceOf[TileKeypad].checkPinAndActivate("0000")
+
+            true
+        }
+        else true
     }
 
     override def canProvidePower: Boolean = true
 
     override def isProvidingStrongPower(world: IBlockAccess, x: Int, y: Int, z: Int, meta: Int): Int = isProvidingWeakPower(world, x, y, z, meta)
 
-    override def isProvidingWeakPower(world: IBlockAccess, x: Int, y: Int, z: Int, meta: Int): Int = 15
+    override def isProvidingWeakPower(world: IBlockAccess, x: Int, y: Int, z: Int, meta: Int): Int = {
+        val tile = world.getTileEntity(x, y, z).asInstanceOf[TileKeypad]
+
+        if (MinecraftServer.getServer.getTickCounter - tile.lastActivatedTick <= 20) 15
+        else 0
+    }
 
     override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, placer: EntityLivingBase, stack: ItemStack) {
         super.onBlockPlacedBy(world, x, y, z, placer, stack)
@@ -45,6 +65,19 @@ class BlockKeypad extends Block(Material.rock) {
             case 3 => ForgeDirection.EAST
 
             case _ => ForgeDirection.NORTH
+        }
+    }
+
+    override def getCollisionBoundingBoxFromPool(world: World, x: Int, y: Int, z: Int): AxisAlignedBB = null
+
+    override def getSelectedBoundingBoxFromPool(world: World, x: Int, y: Int, z: Int): AxisAlignedBB = {
+        world.getTileEntity(x, y, z).asInstanceOf[TileKeypad].facing match {
+            case ForgeDirection.NORTH => AxisAlignedBB.getBoundingBox(x + 0.22, y + 0.05, z, x + 0.78, y + 0.9, z + 0.1)
+            case ForgeDirection.SOUTH => AxisAlignedBB.getBoundingBox(x + 0.22, y + 0.05, z + 0.90, x + 0.78, y + 0.9, z + 1)
+            case ForgeDirection.EAST => AxisAlignedBB.getBoundingBox(x + 0.90, y + 0.05, z + 0.22, x + 1, y + 0.9, z + 0.78)
+            case ForgeDirection.WEST => AxisAlignedBB.getBoundingBox(x, y + 0.05, z + 0.22, x + 0.1, y + 0.9, z + 0.78)
+
+            case _ => super.getSelectedBoundingBoxFromPool(world, x, y, z)
         }
     }
 }
--- src/main/scala/net/cazzar/mods/locks/blocks/tile/TileKeypad.scala
+++ src/main/scala/net/cazzar/mods/locks/blocks/tile/TileKeypad.scala
@@ -1,8 +1,48 @@
 package net.cazzar.mods.locks.blocks.tile
 
-import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
+import net.cazzar.corelib.tile.SyncedTileEntity
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.server.MinecraftServer
 
-class TileKeypad extends TileEntity{
-  var facing = ForgeDirection.NORTH
+class TileKeypad extends SyncedTileEntity {
+    var facing = ForgeDirection.NORTH
+    var lastActivatedTick = -20
+    var pinNumber = "0000"
+
+    override def addExtraNBTToPacket(tag: NBTTagCompound) {}
+
+    override def readExtraNBTFromPacket(tag: NBTTagCompound) {}
+
+    override def updateEntity(): Unit = {
+        super.updateEntity()
+
+        if (MinecraftServer.getServer.getTickCounter - lastActivatedTick > 20) {
+            worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, worldObj.getBlock(xCoord, yCoord, zCoord))
+            worldObj.notifyBlocksOfNeighborChange(xCoord + facing.offsetX, yCoord + facing.offsetY, zCoord + facing.offsetZ, worldObj.getBlock(xCoord, yCoord, zCoord))
+        }
+    }
+
+    override def readFromNBT(tag: NBTTagCompound): Unit = {
+        super.readFromNBT(tag)
+        facing = ForgeDirection.valueOf(tag.getString("facing"))
+        pinNumber = tag.getString("pinNumber")
+    }
+
+    override def writeToNBT(tag: NBTTagCompound): Unit = {
+        super.writeToNBT(tag)
+        tag.setString("facing", facing.name())
+        tag.setString("pinNumber", pinNumber)
+    }
+
+    def checkPinAndActivate(pin: String): Boolean = {
+        if (pin == pinNumber) {
+            lastActivatedTick = MinecraftServer.getServer.getTickCounter
+            worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, worldObj.getBlock(xCoord, yCoord, zCoord))
+            worldObj.notifyBlocksOfNeighborChange(xCoord + facing.offsetX, yCoord + facing.offsetY, zCoord + facing.offsetZ, worldObj.getBlock(xCoord, yCoord, zCoord))
+
+            true
+        }
+        else false
+    }
 }
